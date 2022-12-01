e32731bf04467d95eda82719052a86c4aa60841e
Tamás B. Nemes <tb.nemes@gmail.com>
2022-12-01 09:01:09 +0100

    components

--- get-organised/src/app/app.module.ts
+++ get-organised/src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotesComponent } from './notes/notes.component';
+import { calendarComponent, CalendarComponent } from './calendar/calendar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NotesComponent, calendarComponent, CalendarComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
